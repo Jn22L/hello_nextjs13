f8fc1f299f27fd9d14092f2f8e0357fc9204a220
Jn22L <peaceofjn@gmail.com>
2023-04-30 02:59:12 +0900

    .

--- app/page.js
+++ app/page.js
@@ -3,10 +3,7 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <div>
-      <h1>Next.js 13 공부 메인페이지</h1>
-      <Link href="/mongodb-list">
-        <h4>MongoDB 글목록 보기</h4>
-      </Link>
+      <h1>Next.js 13 으로 게시판 만들기</h1>
     </div>
   );
 }
