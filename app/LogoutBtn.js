9093961610c2b38f5c5f82bfa3fc1e6db57833cd
Jn22L <peaceofjn@gmail.com>
2023-04-30 18:11:07 +0900

    login버튼div제거 한줄로나오게

--- app/LogoutBtn.js
+++ app/LogoutBtn.js
@@ -3,14 +3,12 @@ import { signOut } from "next-auth/react";
 
 export default function LoginBtn() {
   return (
-    <div>
-      <button
-        onClick={() => {
-          signOut();
-        }}
-      >
-        로그아웃
-      </button>
-    </div>
+    <button
+      onClick={() => {
+        signOut();
+      }}
+    >
+      로그아웃
+    </button>
   );
 }
