33473e8234b8a839b23183a8bf4ade3a6cf90809
unknown <22220252@KTNG.AD>
2023-05-02 13:31:47 +0900

    아이디/비번 + JWT 로그인 개발중

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -3,12 +3,13 @@ const url = process.env.MONGODB_URL;
 const options = { useNewUrlParser: true };
 let connectDB;
 
-if (process.env.NODE_ENV === "development") {
-  if (!global._mongo) {
-    global._mongo = new MongoClient(url, options).connect();
-  }
-  connectDB = global._mongo;
-} else {
-  connectDB = new MongoClient(url, options).connect();
-}
+// if (process.env.NODE_ENV === "development") {
+//   if (!global._mongo) {
+//     global._mongo = new MongoClient(url, options).connect();
+//   }
+//   connectDB = global._mongo;
+// } else {
+//   connectDB = new MongoClient(url, options).connect();
+// }
+connectDB = new MongoClient(url, options).connect();
 export { connectDB };
