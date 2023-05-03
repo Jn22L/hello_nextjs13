33dda8167efbfb74f5478f49f184cf5ce1f4a3d7
Jn22L <peaceofjn@gmail.com>
2023-05-03 14:35:00 +0900

    댓글조회저장refactoring

--- app/mongodb-detail/[id]/Comment.js
+++ app/mongodb-detail/[id]/Comment.js
@@ -8,19 +8,17 @@ export default function Comment({ parentId, comments }) {
 
   console.log("댓글의 parentID:", parentId);
 
-  async function fetchApiCommentList(param) {
-    const response = await fetch(`/api/comment/list?parentId=${param.parentId}`);
-    const json = await response.json();
-    if (!response.ok) {
-      throw new Error(JSON.stringify(json));
+  function selectCommentList(pObj) {
+    async function fetchSelect() {
+      const response = await fetch(`/api/comment/list?parentId=${pObj.parentId}`);
+      const json = await response.json();
+      if (!response.ok) {
+        throw new Error(JSON.stringify(json));
+      }
+      return json;
     }
-    return json;
-  }
-
-  function selectCommentList(param) {
-    fetchApiCommentList(param)
+    fetchSelect()
       .then((result) => {
-        console.log("조회결과", result);
         setCommentList(result);
       })
       .catch((error) => {
@@ -28,36 +26,44 @@ export default function Comment({ parentId, comments }) {
       });
   }
 
+  function insertComment(pObj) {
+    async function fetchInsert() {
+      const response = await fetch("/api/comment/new", { method: "POST", body: JSON.stringify({ comment: pObj.comment, parentId: pObj.parentId }) });
+      const json = await response.json();
+      if (!response.ok) {
+        throw new Error(JSON.stringify(json));
+      }
+      return json;
+    }
+    fetchInsert()
+      .then((result) => {
+        selectCommentList({ parentId: pObj.parentId });
+        setComment("");
+        inputRef.current.focus();
+      })
+      .catch((error) => {
+        alert(JSON.parse(error.message).retMsg);
+      });
+  }
+
   useEffect(() => {
-    console.log("컴포넌트가 화면에 나타남");
     selectCommentList({ parentId });
-    return () => {
-      console.log("컴포넌트가 화면에서 사라짐");
-    };
   }, []);
 
   return (
     <div>
       <hr></hr>
       {commentList.length > 0 ? commentList.map((row, idx) => <p key={row._id}>{row.content}</p>) : "댓글없음"}
-
       <input
+        value={comment}
+        ref={inputRef}
         onChange={(e) => {
           setComment(e.target.value);
         }}
-        value={comment}
-        ref={inputRef}
       />
       <button
         onClick={(e) => {
-          console.log(comment);
-          fetch("/api/comment/new", { method: "POST", body: JSON.stringify({ comment, parentId: parentId }) })
-            .then((r) => r.json())
-            .then((result) => {
-              selectCommentList({ parentId });
-              setComment("");
-              inputRef.current.focus();
-            });
+          insertComment({ comment, parentId });
         }}
       >
         댓글전송
