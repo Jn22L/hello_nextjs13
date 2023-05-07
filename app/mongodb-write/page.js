71fd9882ee3e04e0a500b1f85c8ef73a9d61d6ac
Jn22L <peaceofjn@gmail.com>
2023-05-07 14:08:59 +0900

    image미리보기추가-서버저장은아직

--- app/mongodb-write/page.js
+++ app/mongodb-write/page.js
@@ -1,12 +1,24 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
 
 export default function Write() {
+  const [imgUrl, setImgUrl] = useState("");
+
+  function handleChange(e) {
+    const file = e.target.files[0];
+    let src = URL.createObjectURL(file);
+    setImgUrl(src);
+  }
+
   return (
     <div className="p-20">
       <h4>글작성</h4>
       <form action="/api/post/new" method="POST">
         <input name="title" placeholder="글제목"></input>
         <input name="content" placeholder="글내용"></input>
+        <input type="file" accept="image/*" onChange={(e) => handleChange(e)}></input>
+        <img src={imgUrl} />
         <button type="submit">저장</button>
       </form>
     </div>
